a7b43d8c17d1c9b8c46ad138c8a390c07d6f88ef
Lucas Tannus <lucastannus@lucastannus.mac.hpicorp.net>
2022-12-13 12:28:33 -0300

    feat: split to a microservice application and create the docker images supplies

--- udagram-api-user/src/server.ts
+++ udagram-api-user/src/server.ts
@@ -1,16 +1,15 @@
 import cors from 'cors';
 import express from 'express';
-import {sequelize} from './sequelize';
+import { sequelize } from './sequelize';
 
-import {IndexRouter} from './controllers/v0/index.router';
+import { IndexRouter } from './controllers/v0/index.router';
 
 import bodyParser from 'body-parser';
-import {config} from './config/config';
-import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
+import { config } from './config/config';
+import { V0_USER_MODELS } from './controllers/v0/model.index';
 
 
 (async () => {
-  await sequelize.addModels(V0_FEED_MODELS);
   await sequelize.addModels(V0_USER_MODELS);
 
   console.debug("Initialize database connection...");
